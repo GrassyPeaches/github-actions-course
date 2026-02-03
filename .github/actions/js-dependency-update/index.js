9a15bbfdf88987102a08f714118e9e9b1a982ac7
GrassyPeaches <gracirami97@gmail.com>
2026-02-02 21:00:48 -0800

    feat(17-custom-actions): parse and validate inputs

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,13 +1,57 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+const validateBranchName = ({ branchName }) => /^[ a-zA-Z0-9_\-\.\/ ]+$/.test(branchName);
+const validateDirectoryName = ({ dirName }) => /^[ a-zA-Z0-9_\-\/ ]+$/.test(dirName);
 
 async function run() {
+const baseBranch = core.getInput('base-branch');
+const targetBranch = core.getInput('target-branch');
+const ghToken = core.getInput('gh-token');
+const workingDir = core.getInput('working-directory');
+const debug = core.getBooleanInput('false');
+
+core.setSecret(ghToken);
+
+if(!validateBranchName({branchName: baseBranch})) {
+  core.setFailed('Invalid base branch name. Branch names should include only characters, numbers, hyphens, underscores, dots and forward slashes.')
+return;
+}
+
+if(!validateBranchName({branchName: targetBranchBranch})) {
+  core.setFailed('Invalid target branch name. Branch names should include only characters, numbers, hyphens, underscores, dots and forward slashes.')
+return;
+}
+
+if(!validateDirectoryName({dirNameName: workingDir})) {
+  core.setFailed('Invalid working directory name. Directory names should include only characters, numbers, hyphens, underscores, and forward slashes.')
+return;
+}
+
+core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
+core.info(`[js-dependency-update] : target branch is ${targetBranch}`);
+core.info(`[js-dependency-update] : working directory is ${workingDir}`);
+
+await exec.exec('npm update', [], {
+  cwd: workingDir
+});
+
+const gitStatus = await exec.getExecOutput('git status -s package*.json', [], {
+  cwd: workingDir
+});
+
+if(gitStatus.stdout.length > 0){
+  core.info('[js-dependency-update] - There are updates available!')
+} else{
+  core.info('[js-dependency-update] - No updates at this point in time.')
+}
 /*
-  1.Parse inputs: 
+  [DONE]1.Parse inputs: 
       1.1 base branch from which to check updates
       1.2 target branch to use to create the PR
       1.3 Github token for authentication (to create PRs for example)
       1.4 Working directory for which to check dependencies
-  2. Execute the npm udate command within the working directory
+  [DONE]2. Execute the npm udate command within the working directory
   3. Check whether there are modified package*.json files
   4. If there are modified files: 
     4.1 Add and commit files to the target-branch
